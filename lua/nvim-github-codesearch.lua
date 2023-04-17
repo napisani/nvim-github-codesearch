1523ca6ab8cd6d06d1ad1666e9235effeb0442d2
Nick Pisani <napisani@yahoo.com>
2023-04-17 12:47:51 -0400

    working async with telescope + native neovim ui

--- lua/nvim-github-codesearch.lua
+++ lua/nvim-github-codesearch.lua
@@ -10,142 +10,164 @@ local github_api_url
 local use_telescope = false
 local lib_gh_search = require("libgithub_search")
 
-local read_to_str = function(file)
-	local f = assert(io.open(file, "rb"))
-	local content = f:read("*all")
-	f:close()
-	return content
-end
-
 ---@param config Config the configuration object
 M.setup = function(config)
-	if not config then
-		return
-	end
-	github_api_url = config.github_api_url or "https://api.github.com"
-	github_auth_token = config.github_auth_token or os.getenv("GITHUB_AUTH_TOKEN")
-	if not github_auth_token then
-		error(
-			"github api token not found. please set it in the config or in the GITHUB_AUTH_TOKEN environment variable or configure the github_auth_token when calling setup(...)"
-		)
-		return
-	end
-	lib_gh_search = require("libgithub_search")
+  if not config then
+    return
+  end
+  github_api_url = config.github_api_url or "https://api.github.com"
+  github_auth_token = config.github_auth_token or os.getenv("GITHUB_AUTH_TOKEN")
+  if config.use_telescope ~= nil then
+    use_telescope = config.use_telescope
+  end
+  if not github_auth_token then
+    error(
+      "github api token not found. please set it in the config or in the GITHUB_AUTH_TOKEN environment variable or configure the github_auth_token when calling setup(...)"
+    )
+    return
+  end
+  lib_gh_search = require("libgithub_search")
 end
 
-local function select_with_native_ui(results)
-	local names = {}
-	for _, result_item in ipairs(results) do
-		table.insert(names, result_item["result_entry_full_name"])
-	end
-	-- table.sort(names)
-	vim.ui.select(names, { prompt = "Select github result: " }, function(selected)
-		if selected then
-			print(selected)
-			-- M.set_current(selected)
-		end
-	end)
+local function result_item_to_qflist_entry(item)
+  return {
+    -- bufnr = entry.bufnr,
+    filename = item.downloaded_local_path,
+    lnum = 1,
+    col = 1,
+    text = item.result_entry_full_name,
+  }
 end
-
-local search_cb_jump = function(self, bufnr, query)
+local function send_all_to_qf(results)
+  local qf_entries = {}
+  for _, result_item in ipairs(results) do
+    table.insert(qf_entries, result_item_to_qflist_entry(result_item))
+  end
+  vim.fn.setqflist(qf_entries, ' ')
+  local qf_title = string.format([[github results: (%s)]], 'term')
+  vim.fn.setqflist({}, "a", { title = qf_title })
+  vim.cmd("copen")
+end
+local telescope_search_cb_jump = function(self, bufnr, query)
   if not query then
     return
   end
   vim.api.nvim_buf_call(bufnr, function()
     pcall(vim.fn.matchdelete, self.state.hl_id, self.state.winid)
-    vim.cmd "norm! gg"
+    vim.cmd("norm! gg")
     vim.fn.search(query, "W")
-    vim.cmd "norm! zz"
-
+    vim.cmd("norm! zz")
     self.state.hl_id = vim.fn.matchadd("TelescopePreviewMatch", query)
   end)
 end
-
-local function select_with_telescope(results)
-	local pickers = require("telescope.pickers")
-	local actions = require("telescope.actions")
-	local action_state = require("telescope.actions.state")
-	local previewers = require("telescope.previewers")
-	local finders = require("telescope.finders")
-	local conf = require("telescope.config").values
-	pickers
-		.new({}, {
-			prompt_title = "Select github search result: ",
-			finder = finders.new_table({
-				results = results,
-				entry_maker = function(item)
-					return {
-						value = item.result_entry_full_name,
-						ordinal = item.result_entry_full_name,
-						display = item.result_entry_full_name,
-						item = item
-					}
-				end,
-			}),
-			sorter = conf.file_sorter({}),
-			previewer = previewers.new_buffer_previewer({
-				title = "Results",
-				define_preview = function(self, entry)
-					if not entry or not entry.item then
-						return
-					end
-					local item = entry.item
-					-- local download_result = lib_gh_search.download({
-					-- 	download_url = item.url,
-					-- 	filename = item.name,
-					-- 	token = github_auth_token,
-					-- })
-					-- if download_result == nil then
-					-- 	error("an unknown error occurred while downloading file from github")
-					-- 	return
-					-- end
-					-- if download_result["error"] ~= nil then
-					-- 	error(results["error"])
-					-- 	return
-					-- end
-					conf.buffer_previewer_maker(item.downloaded_local_path, self.state.bufnr, {
-						bufname = self.state.bufname,
-						callback = function(bufnr)
-              
-              search_cb_jump(self, bufnr, item.original_search_term)
-						end,
-					})
-				end,
-			}),
-			attach_mappings = function(prompt_bufnr)
-				actions.select_default:replace(function()
-					actions.close(prompt_bufnr)
-					local selection = action_state.get_selected_entry()
-					if selection then
-						-- print(vim.inspect(selection))
-						-- M.set_current(selection.value)
-					end
-				end)
-				return true
-			end,
-		})
-		:find()
+local function notify(err, level_in)
+  if err then
+    if level_in == nil then
+      level_in = "ERROR"
+    end
+    local level = vim.log.levels[level_in]
+    vim.notify(err, level, {
+      title = "nvim-github-codesearch",
+    })
+    return true
+  end
+  return false
 end
 
+local function select_with_telescope(results)
+  local pickers = require("telescope.pickers")
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+  local previewers = require("telescope.previewers")
+  local finders = require("telescope.finders")
+  local conf = require("telescope.config").values
+  pickers
+      .new({}, {
+        prompt_title = "Select github search result: ",
+        finder = finders.new_table({
+          results = results,
+          entry_maker = function(item)
+            return {
+              value = item.downloaded_local_path,
+              text = item.result_entry_full_name,
+              ordinal = item.result_entry_full_name,
+              display = item.result_entry_full_name,
+              item = item,
+            }
+          end,
+        }),
+        sorter = conf.file_sorter({}),
+        previewer = previewers.new_buffer_previewer({
+          title = "Results",
+          define_preview = function(self, entry)
+            if not entry or not entry.item then
+              return
+            end
+            local item = entry.item
+            if item["error"] ~= nil then
+              notify(item["error"])
+              return
+            end
+            conf.buffer_previewer_maker(item.downloaded_local_path, self.state.bufnr, {
+              bufname = self.state.bufname,
+              callback = function(bufnr)
+                telescope_search_cb_jump(self, bufnr, item.original_search_term)
+              end,
+            })
+          end,
+        }),
+        attach_mappings = function(prompt_bufnr)
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            if selection then
+              if selection.item["error"] ~= nil then
+                notify(selection.item["error"])
+                return
+              end
+              vim.cmd(":edit " .. selection.item.downloaded_local_path)
+            end
+          end)
+          return true
+        end,
+      })
+      :find()
+end
+-- search github for the given query. If use_telescope is true, use telescope to display results. 
+-- Otherwise, display results in the quickfix list.
+-- @param query string the search term + restrictions to use
 M.search = function(query)
-	local results = lib_gh_search.request_codesearch({
-		query = query,
-		token = github_auth_token,
-		url = github_api_url,
-	})
-	if results == nil then
-		error("an unknown error occurred while searching github code")
-		return
-	end
-	if results["error"] ~= nil then
-		error(results["error"])
-		return
-	end
-	select_with_telescope(results)
-	return results
-	-- vim.ui.input({ prompt = 'Search GitHub code:' }, function(input)
-	--   print(input)
-	-- end)
+  local results = lib_gh_search.request_codesearch({
+    query = query,
+    token = github_auth_token,
+    url = github_api_url,
+  })
+  if results == nil then
+    notify("an unknown error occurred while searching github code")
+    return
+  end
+  if results["error"] ~= nil then
+    notify(results["error"])
+    return
+  end
+  if use_telescope then
+    select_with_telescope(results)
+  else
+    send_all_to_qf(results)
+  end
+  return results
+end
+-- prompt the user for a search term + restrictions. Then, search github and display results.
+M.prompt = function()
+  local input = vim.fn.input("Search GitHub code: ")
+  if input == nil or input == "" then
+    return
+  end
+  M.search(input)
+end
+-- clean up any tempfiles created by nvim-github-codesearch
+M.cleanup = function()
+  lib_gh_search.cleanup()
 end
 
 return M
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
 mod query;
+use futures::future;
 use mlua::prelude::{Lua, LuaResult, LuaTable};
 use reqwest::header;
 use serde::{Deserialize, Serialize};
-use std::{env, error::Error, fs::File, io::Cursor, path::PathBuf, thread};
+use std::{
+    env,
+    error::Error,
+    fs::{self, File},
+    io::Cursor,
+    path::PathBuf,
+};
+use tokio::task::JoinHandle;
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize, Serialize)]
 struct SearchResults {
@@ -39,7 +47,7 @@ struct DownloadResponse {
     download_url: String,
 }
 
-fn get_github_request_client(token: &str) -> Result<reqwest::blocking::Client, Box<dyn Error>> {
+fn get_github_request_client(token: &str) -> Result<reqwest::Client, Box<dyn Error>> {
     let mut headers = header::HeaderMap::new();
     let token_header = format!("Bearer {}", token);
     headers.insert(
@@ -55,22 +63,33 @@ fn get_github_request_client(token: &str) -> Result<reqwest::blocking::Client, B
         "User-Agent",
         header::HeaderValue::from_static("nvim-github-codesearch"),
     );
-    reqwest::blocking::Client::builder()
+    reqwest::Client::builder()
         .default_headers(headers)
         .build()
         .map_err(|e| Box::new(e) as Box<dyn Error>)
 }
-fn download_file(url: &str, filename: &str, token: &str) -> Result<PathBuf, Box<dyn Error>> {
-    let client = get_github_request_client(token)?;
-    let dir = env::temp_dir(); //.join("nvimghs");
-                               //TODO
-                               // if ! dir.exists() {
-                               //     dir.create_dir()?;
-                               // }
-    let res = client.get(url).send()?;
+
+fn cleanup_temp_files() -> Result<(), Box<dyn Error>> {
+    let dir = env::temp_dir().join("nvimghs");
+    if dir.exists() {
+        fs::remove_dir_all(dir)?;
+    }
+    Ok(())
+}
+
+async fn download_file<'a, 'b>(
+    url: String,
+    filename: String,
+    token: String,
+) -> Result<PathBuf, Box<dyn Error>> {
+    let client = get_github_request_client(&token)?;
+    let dir = env::temp_dir().join("nvimghs");
+    fs::create_dir_all(&dir)?;
+    let res = client.get(&url).send().await?;
     if !res.status().is_success() {
         let search_error = res
             .json::<GithubAPIError>()
+            .await
             .expect("failed to parse error from github download response");
         return Err(Box::new(std::io::Error::new(
             std::io::ErrorKind::Other,
@@ -79,8 +98,9 @@ fn download_file(url: &str, filename: &str, token: &str) -> Result<PathBuf, Box<
     }
     let download_res = res
         .json::<DownloadResponse>()
+        .await
         .expect("failed to parse json from github download response");
-    let res = client.get(download_res.download_url).send()?;
+    let res = client.get(download_res.download_url).send().await?;
     let digest = md5::compute(url);
     let temp_file_name = format!("{:x}-{}", digest, filename);
     let path = dir.join(temp_file_name);
@@ -88,11 +108,11 @@ fn download_file(url: &str, filename: &str, token: &str) -> Result<PathBuf, Box<
         return Ok(path);
     }
     let mut file = File::create(path.clone())?;
-    let mut content = Cursor::new(res.bytes().unwrap());
+    let mut content = Cursor::new(res.bytes().await.unwrap());
     std::io::copy(&mut content, &mut file)?;
     Ok(path)
 }
-fn request_codesearch(
+async fn request_codesearch(
     query: &str,
     url: &str,
     token: &str,
@@ -101,96 +121,125 @@ fn request_codesearch(
     let query = urlencoding::encode(query);
     let res = client
         .get(format!("{}/search/code?q={}", url, query))
-        .send()?;
+        .send()
+        .await?;
     if !res.status().is_success() {
         let search_error = res
             .json::<GithubAPIError>()
+            .await
             .expect("failed to parse error from github search response");
         return Err(Box::new(std::io::Error::new(
             std::io::ErrorKind::Other,
             format!("Github responded with an error: {}", search_error.message),
         )));
     }
-    // println!("requesting {}", res.text().unwrap());
     let results = res
         .json::<SearchResults>()
+        .await
         .expect("failed to parse json from github search response");
     Ok(results)
 }
 
+#[tokio::main]
+async fn search_and_download_results<'a>(
+    lua: &'a Lua,
+    args: LuaTable,
+) -> Result<LuaTable<'a>, Box<dyn Error>> {
+    let query = args.get::<_, String>("query")?;
+    let url = args.get::<_, String>("url")?;
+    let token = args.get::<_, String>("token")?;
+    let parsed_query = query::SearchQuery::from_query_string(&query);
+    if let Err(err) = parsed_query {
+        let lua_table = lua.create_table()?;
+        lua_table.set("error", err.to_string())?;
+        return Ok(lua_table);
+    }
+    let parsed_query = parsed_query.unwrap();
+    let results = request_codesearch(&query, &url, &token).await;
+
+    let lua_table = lua.create_table()?;
+    if let Err(err) = results {
+        lua_table.set("error", err.to_string())?;
+        return Ok(lua_table);
+    }
+    let results = results.unwrap();
+    let tasks: Vec<JoinHandle<_>> = results
+        .items
+        .iter()
+        .map(|item| {
+            (
+                item.url.to_string(),
+                item.name.to_string(),
+                token.to_string(),
+            )
+        })
+        .map(|(url, name, token)| {
+            tokio::spawn(async move {
+                download_file(url.to_string(), name, token)
+                    .await
+                    .map_err(|e| {
+                        std::io::Error::new(
+                            std::io::ErrorKind::Other,
+                            format!("failed to download file from url:{} {}", url, e),
+                        )
+                    })
+            })
+        })
+        .collect::<Vec<_>>();
+    let task_results = future::join_all(tasks).await;
+    for (i, item) in results.items.iter().enumerate() {
+        let lua_item = lua.create_table()?;
+        lua_item.set("name", item.name.to_string())?;
+        lua_item.set("path", item.path.to_string())?;
+        lua_item.set("sha", item.sha.to_string())?;
+        lua_item.set("url", item.url.to_string())?;
+        lua_item.set("git_url", item.git_url.to_string())?;
+        lua_item.set("html_url", item.html_url.to_string())?;
+        lua_item.set("score", item.score)?;
+        lua_item.set("original_search_term", parsed_query.search_term.to_string())?;
+        // this is the name display value for all search result items
+        lua_item.set(
+            "result_entry_full_name",
+            format!("{}: {}", item.repository.full_name, item.path),
+        )?;
+        let download = task_results
+            .get(i)
+            .unwrap()
+            .as_ref()
+            .expect("failed to get refrence from task result");
+        if let Err(err) = download {
+            lua_item.set("error", err.to_string())?;
+        } else {
+            let download = download
+                .as_ref()
+                .expect("failed to get refrence from task result");
+            // this is the path to the downloaded file to be used in previews / buffer open commands
+            lua_item.set("downloaded_local_path", download.to_str())?;
+        }
+        lua_table.set(i + 1, lua_item)?;
+    }
+    Ok(lua_table)
+}
 #[mlua::lua_module]
 fn libgithub_search(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
+    let cleanup_fn = lua
+        .create_function(|_, ()| {
+            cleanup_temp_files();
+            Ok(())
+        })
+        .unwrap();
+    exports
+        .set("cleanup", cleanup_fn)
+        .expect("failed to create lua cleanup_fn");
     let search_fn = lua
-        .create_async_function(async |lua, args: LuaTable| {
-            let query = args.get::<_, String>("query")?;
-            let url = args.get::<_, String>("url")?;
-            let token = args.get::<_, String>("token")?;
-            let parsed_query = query::SearchQuery::from_query_string(&query);
-            if let Err(err) = parsed_query {
-                let lua_table = lua.create_table()?;
-                lua_table.set("error", err.to_string())?;
-                return Ok(lua_table);
-            }
-            let parsed_query = parsed_query.unwrap();
-            let results = request_codesearch(&query, &url, &token);
-
-            let lua_table = lua.create_table()?;
-            if let Err(err) = results {
-                lua_table.set("error", err.to_string())?;
-                return Ok(lua_table);
-            }
-            let results = results.unwrap();
-            for (i, item) in results.items.iter().enumerate() {
-                let lua_item = lua.create_table()?;
-                lua_item.set("name", item.name.to_string())?;
-                lua_item.set("path", item.path.to_string())?;
-                lua_item.set("sha", item.sha.to_string())?;
-                lua_item.set("url", item.url.to_string())?;
-                lua_item.set("git_url", item.git_url.to_string())?;
-                lua_item.set("html_url", item.html_url.to_string())?;
-                lua_item.set("score", item.score)?;
-                lua_item.set("original_search_term", parsed_query.search_term.to_string())?;
-                let download = download_file(&item.url, &item.name, &token).unwrap();
-                lua_item.set(
-                    "downloaded_local_path",
-                        download.to_str()
-                )?;
-                lua_item.set(
-                    "result_entry_full_name",
-                    format!("{}: {}", item.repository.full_name, item.path),
-                )?;
-                lua_table.set(i + 1, lua_item)?;
-            }
-
-            // results.items.iter().for_each(|item| {
-            // });
-            Ok(lua_table)
+        .create_async_function(|lua, args: LuaTable| async move {
+            Ok(search_and_download_results(lua, args).unwrap())
         })
         .expect("failed to create lua search_fn");
-
-    let download_fn = lua
-        .create_function(|lua, args: LuaTable| {
-            let download_url = args.get::<_, String>("download_url")?;
-            let filename = args.get::<_, String>("filename")?;
-            let token = args.get::<_, String>("token")?;
-            let results = download_file(&download_url, &filename, &token);
-            let lua_table = lua.create_table()?;
-            if let Err(err) = results {
-                lua_table.set("error", err.to_string())?;
-                return Ok(lua_table);
-            }
-            let results = results.unwrap();
-            lua_table.set("path", results.to_str().unwrap())?;
-            Ok(lua_table)
-        })
-        .expect("failed to create lua download_fn");
     exports
         .set("request_codesearch", search_fn)
         .expect("failed to set search_fn on exported lua table");
-    exports
-        .set("download", download_fn)
-        .expect("failed to set download_fn on exported lua table");
     Ok(exports)
 }
 
@@ -198,18 +247,23 @@ fn libgithub_search(lua: &Lua) -> LuaResult<LuaTable> {
 mod tests {
     use super::*;
     use std::env;
-    // #[test]
-    // fn request_codesearch_returns_results() {
-    //     let result =
-    //         request_codesearch("test", "https://api.github.com", env!("GIT_NPM_AUTH_TOKEN"));
-    //     assert!(result.is_ok());
-    // }
-    // #[test]
-    // fn download_file_returns_path() {
-    //     let url = "https://api.github.com/repositories/683527/contents/test.data/positiveUpdate/ObsInjectionsData.test?ref=9fe69622a29a3f1a230254018271a3187b54db56";
-    //     let result = download_file(url, "manifest.txt", env!("GIT_NPM_AUTH_TOKEN"));
-    //     println!("result: {:?}", result);
-    //     assert!(result.is_ok());
-    // }
+    #[tokio::test]
+    async fn request_codesearch_returns_results() {
+        let result =
+            request_codesearch("test", "https://api.github.com", env!("GIT_NPM_AUTH_TOKEN")).await;
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn download_file_returns_path() {
+        let url = "https://api.github.com/repositories/683527/contents/test.data/positiveUpdate/ObsInjectionsData.test?ref=9fe69622a29a3f1a230254018271a3187b54db56";
+        let result = download_file(
+            url.to_string(),
+            "manifest.txt".to_string(),
+            env!("GIT_NPM_AUTH_TOKEN").to_string(),
+        )
+        .await;
+        println!("result: {:?}", result);
+        assert!(result.is_ok());
+    }
 }
-// :lua require('user.utils').print(require("libgithub_search").scopes)
